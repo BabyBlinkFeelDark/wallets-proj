171a537d282d70b2d2b2363aaf212c2338135fb7
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-17 21:51:12 +0300

    creating view for POST

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -15,3 +15,7 @@ class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
         fields = '__all__'
+
+class WalletOperationSerializer(serializers.Serializer):
+    operation_type = serializers.ChoiceField(choices=[('DEPOSIT', 'DEPOSIT'), ('WITHDRAW', 'WITHDRAW')])
+    amount = serializers.DecimalField(max_digits=20, decimal_places=2)
--- wallets/views.py
+++ wallets/views.py
@@ -25,3 +25,27 @@ class WalletDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
 
+# wallets/views.py
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Wallet, WalletOperation
+from .serializers import WalletOperationSerializer
+from django.db import transaction
+
+@api_view(['POST'])
+def create_wallet_operation(request, wallet_uuid):
+    wallet = Wallet.objects.filter(id=wallet_uuid).first()
+    serializer = WalletOperationSerializer(data=request.data)
+    if serializer.is_valid():
+        operation_type = serializer.validated_data['operation_type']
+        amount = serializer.validated_data['amount']
+        with transaction.atomic():
+            if operation_type == 'DEPOSIT':
+                wallet.balance += amount
+            elif operation_type == 'WITHDRAW':
+                wallet.balance -= amount
+            wallet.save()
+            WalletOperation.objects.create(wallet=wallet, operation_type=operation_type, amount=amount)
+        return Response({'balance': wallet.balance}, status=status.HTTP_201_CREATED)
+
