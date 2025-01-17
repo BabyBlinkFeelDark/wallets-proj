d62c4a7ff394e05f47f13dbd29d6e693eecfa772
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-17 23:20:04 +0300

    весь покрытый тестами, абсолютно весь, ползает в контейнере, модуль мой жиесть

--- wallets/urls.py
+++ wallets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WalletList, WalletDetail, WalletBalanceDetail
+from .views import WalletList, WalletDetail, WalletBalanceDetail, create_wallet_operation
 
 urlpatterns = [
     path('wallets/', WalletList.as_view(), name='wallet-list'),
-    path('wallets/<int:pk>/', WalletDetail.as_view(), name='wallet-detail'),
+    path('wallets/<uuid:pk>/', WalletDetail.as_view(), name='wallet-detail'),
     path('wallets/<uuid:wallet_uuid>/balance/', WalletBalanceDetail.as_view(), name='wallet-balance'),
+    path('wallets/<uuid:wallet_uuid>/operation/', create_wallet_operation, name='wallet-operation'),
 ]
